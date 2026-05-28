2663c802801604789a872f12927cb5cc1270c956
DasariSatyaSaiRohit <dasarisatyasairohit@gmail.com>
2026-05-28 12:47:56 +0530

    Update vite.config.ts

--- breathe-esg_application/vite.config.ts
+++ breathe-esg_application/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'breath-esg-production.up.railway.app',
         changeOrigin: true,
       },
     },
